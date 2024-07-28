0cc952021b3d935411b55477accec6a7a747eae3
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-28 13:08:28 +0530

    Update index.test.tsx

--- index.test.tsx
+++ index.test.tsx
@@ -1,69 +1,137 @@
-import handleResourceExport from './handleResourceExport';
-import parseCookie from 'src/utils/parseCookie';
-import rison from 'rison';
-import { nanoid } from 'nanoid';
+import {
+  ensureIsArray,
+  ExtraFormData,
+  t,
+  TimeGranularity,
+  tn,
+} from '@superset-ui/core';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Select } from 'src/components';
+import { FormItemProps } from 'antd/lib/form';
+import { FilterPluginStyle, StyledFormItem, StatusMessage } from '../common';
+import { PluginFilterTimeGrainProps } from './types';
 
-// Mock the dependencies
-jest.mock('nanoid');
-jest.mock('src/utils/parseCookie');
+export default function PluginFilterTimegrain(
+  props: PluginFilterTimeGrainProps,
+) {
+  const {
+    data,
+    formData,
+    height,
+    width,
+    setDataMask,
+    setHoveredFilter,
+    unsetHoveredFilter,
+    setFocusedFilter,
+    unsetFocusedFilter,
+    setFilterActive,
+    filterState,
+    inputRef,
+  } = props;
+  const { defaultValue } = formData;
 
-describe('handleResourceExport', () => {
-  const resource = 'example_resource';
-  const ids = [1, 2, 3];
-  const token = 'test_token';
-  const done = jest.fn();
-  const interval = 200;
+  const [value, setValue] = useState<string[]>(defaultValue ?? []);
+  const durationMap = useMemo(
+    () =>
+      data.reduce(
+        (agg, { duration, name }: { duration: string; name: string }) => ({
+          ...agg,
+          [duration]: name,
+        }),
+        {} as { [key in string]: string },
+      ),
+    [JSON.stringify(data)],
+  );
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-    nanoid.mockReturnValue(token);
-  });
+  const handleChange = (values: string[] | string | undefined | null) => {
+    const resultValue: string[] = ensureIsArray<string>(values);
+    const [timeGrain] = resultValue;
+    const label = timeGrain ? durationMap[timeGrain] : undefined;
 
-  it('should create an iframe and set its src', () => {
-    const appendChildMock = jest.spyOn(document.body, 'appendChild');
-    const createElementMock = jest.spyOn(document, 'createElement').mockImplementation(() => {
-      return { style: {}, src: '' };
+    const extraFormData: ExtraFormData = {};
+    if (timeGrain) {
+      extraFormData.time_grain_sqla = timeGrain as TimeGranularity;
+    }
+    setValue(resultValue);
+    setDataMask({
+      extraFormData,
+      filterState: {
+        label,
+        value: resultValue.length ? resultValue : null,
+      },
     });
-    handleResourceExport(resource, ids, done, interval);
+  };
 
-    const expectedUrl = `/api/v1/${resource}/export/?q=${rison.encode(ids)}&token=${token}`;
-    expect(createElementMock).toHaveBeenCalledWith('iframe');
-    expect(appendChildMock).toHaveBeenCalled();
-    expect(appendChildMock.mock.calls[0][0].src).toBe(expectedUrl);
-    expect(appendChildMock.mock.calls[0][0].style.display).toBe('none');
-  });
+  useEffect(() => {
+    handleChange(defaultValue ?? []);
+    // I think after Config Modal update some filter it re-creates default value for all other filters
+    // so we can process it like this `JSON.stringify` or start to use `Immer`
+  }, [JSON.stringify(defaultValue)]);
 
-  it('should call done() and remove iframe when cookie is set', () => {
-    const removeChildMock = jest.spyOn(document.body, 'removeChild');
-    const setIntervalMock = jest.spyOn(window, 'setInterval');
-    const clearIntervalMock = jest.spyOn(window, 'clearInterval');
+  useEffect(() => {
+    handleChange(filterState.value ?? []);
+  }, [JSON.stringify(filterState.value)]);
 
-    parseCookie.mockReturnValue({ [token]: 'done' });
+  
 
-    handleResourceExport(resource, ids, done, interval);
+  const formItemData: FormItemProps = {};
+  if (filterState.validateMessage) {
+    formItemData.extra = (
+      <StatusMessage status={filterState.validateStatus}>
+        {filterState.validateMessage}
+      </StatusMessage>
+    );
+  }
+  
+  const options = useMemo(() => {
+    return (data || []).map(
+    (row: { name: string; duration: string }) => {
+      const { name, duration } = row;
+      if ((duration === 'PT1S' && (formData?.pt1S === undefined || formData?.pt1S === true))
+      || (duration === 'PT1M' && (formData?.pt1M === undefined || formData?.pt1M === true))
+      || (duration === 'PT1H' && (formData?.pt1H === undefined || formData?.pt1H === true))
+      || (duration === 'P1D' && (formData?.p1D === undefined || formData?.p1D === true))
+      || (duration === 'P1W' && (formData?.p1W === undefined || formData?.p1W === true))
+      || (duration === 'P1M' && (formData?.p1M === undefined || formData?.p1M === true))
+      || (duration === 'P3M' && (formData?.p3M === undefined || formData?.p3M === true))
+      || (duration === 'P1Y' && (formData?.p1Y === undefined || formData?.p1Y === true))
+      ) {
+        return {
+          label: name,
+          value: duration,
+        };
+      }
+      return undefined
+    },
+  ).filter(notUndefined => notUndefined !== undefined)
+  }, [formData?.pt1S, formData?.pt1M, formData?.pt1H, formData?.p1D, formData?.p1W, formData?.p1M, formData?.p3M, formData?.p1Y]);
 
-    expect(setIntervalMock).toHaveBeenCalled();
+  const placeholderText =
+    (options || []).length === 0
+      ? t('No data')
+      : tn('%s option', '%s options', options.length, options.length);
 
-    const intervalCallback = setIntervalMock.mock.calls[0][0];
-    intervalCallback();
-
-    expect(clearIntervalMock).toHaveBeenCalled();
-    expect(removeChildMock).toHaveBeenCalled();
-    expect(done).toHaveBeenCalled();
-  });
-
-  it('should not call done() or remove iframe when cookie is not set', () => {
-    const removeChildMock = jest.spyOn(document.body, 'removeChild');
-    const setIntervalMock = jest.spyOn(window, 'setInterval');
-
-    parseCookie.mockReturnValue({});
-
-    handleResourceExport(resource, ids, done, interval);
-
-    const intervalCallback = setIntervalMock.mock.calls[0][0];
-    intervalCallback();
-
-    expect(removeChildMock).not.toHaveBeenCalled();
-    expect(done).not.toHaveBeenCalled();
-  });
-});
+  return (
+    <FilterPluginStyle height={height} width={width}>
+      <StyledFormItem
+        validateStatus={filterState.validateStatus}
+        {...formItemData}
+      >
+        <Select
+          allowClear
+          value={value}
+          placeholder={placeholderText}
+          // @ts-ignore
+          onChange={handleChange}
+          onBlur={unsetFocusedFilter}
+          onFocus={setFocusedFilter}
+          onMouseEnter={setHoveredFilter}
+          onMouseLeave={unsetHoveredFilter}
+          ref={inputRef}
+          options={options}
+          onDropdownVisibleChange={setFilterActive}
+        />
+      </StyledFormItem>
+    </FilterPluginStyle>
+  );
+}
