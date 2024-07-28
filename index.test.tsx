187c1394a22fd31bdb876caa65c9b9ecef5f34c9
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-28 13:36:46 +0530

    Update index.test.tsx

--- index.test.tsx
+++ index.test.tsx
@@ -1,21 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -34,9 +16,7 @@ import ControlRow from 'src/explore/components/ControlRow';
 import Control from 'src/explore/components/Control';
 import { controls } from 'src/explore/controls';
 import { getExploreUrl } from 'src/explore/exploreUtils';
-// import { getChartDataRequest } from 'src/chart/chartAction';
 import { getChartDataRequest } from 'src/components/Chart/chartAction';
-// import  sandboxedEval  from 'src/modules/utils';
 import NoResultsComponent from '../../../../packages/superset-ui-core/src/chart/components/NoResultsComponent'
 import './BucketKPI.less';
 import { BsArrowUp } from '@react-icons/all-files/bs/BsArrowUp';
