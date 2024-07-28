d8e8f82664a50be26dd090debea0adfecc158491
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-28 23:39:49 +0530

    Update index.test.tsx

--- index.test.tsx
+++ index.test.tsx
@@ -1,108 +1,135 @@
-// ControlFormItem.test.jsx
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { useTheme } from '@superset-ui/core';
-import ControlFormItem, { ControlFormItemComponents } from './ControlFormItem';
-import ControlHeader from '../ControlHeader';
-import InfoTooltipWithTrigger from '../InfoTooltipWithTrigger';
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { FunctionComponentElement, useMemo } from 'react';
+import {
+  FAST_DEBOUNCE,
+  JsonObject,
+  JsonValue,
+  useTheme,
+} from '@superset-ui/core';
+import { debounce } from 'lodash';
+import { ControlFormItemNode } from './ControlFormItem';
 
-// Mock necessary modules
-jest.mock('@superset-ui/core', () => ({
-  useTheme: jest.fn(),
-}));
+export * from './ControlFormItem';
 
-jest.mock('../ControlHeader', () => jest.fn(() => <div>ControlHeader</div>));
-jest.mock('../InfoTooltipWithTrigger', () => jest.fn(() => <div>Tooltip</div>));
+export type ControlFormRowProps = {
+  children: ControlFormItemNode | ControlFormItemNode[];
+};
 
-const MockControlComponent = ({ value, onChange }) => (
-  <input
-    data-testid="mock-control"
-    value={value}
-    onChange={e => onChange(e.target.value)}
-  />
-);
+export function ControlFormRow({ children }: ControlFormRowProps) {
+  const { gridUnit } = useTheme();
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        margin: -2 * gridUnit,
+        marginBottom: gridUnit,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
 
-ControlFormItemComponents.Checkbox = ({ children, checked, onChange }) => (
-  <div>
-    <input
-      type="checkbox"
-      checked={checked}
-      onChange={e => onChange(e)}
-    />
-    {children}
-  </div>
-);
+type ControlFormRowNode = FunctionComponentElement<ControlFormRowProps>;
 
-ControlFormItemComponents.Select = MockControlComponent;
+export type ControlFormProps = {
+  /**
+   * Form field values dict.
+   */
+  value?: JsonObject;
+  onChange: (value: JsonObject) => void;
+  children: ControlFormRowNode | ControlFormRowNode[];
+};
 
-describe('ControlFormItem', () => {
-  beforeEach(() => {
-    useTheme.mockReturnValue({ gridUnit: 4 });
+/**
+ * Light weight form for control panel.
+ */
+export default function ControlForm({
+  onChange,
+  value,
+  children,
+}: ControlFormProps) {
+  const theme = useTheme();
+  const debouncedOnChange = useMemo(
+    () =>
+      ({
+        0: onChange,
+        [FAST_DEBOUNCE]: debounce(onChange, FAST_DEBOUNCE),
+      } as Record<number, typeof onChange>),
+    [onChange],
+  );
+
+  const updatedChildren = React.Children.map(children, row => {
+    if ('children' in row.props) {
+      const defaultWidth = Array.isArray(row.props.children)
+        ? `${100 / row.props.children.length}%`
+        : undefined;
+      return React.cloneElement(row, {
+        children: React.Children.map(row.props.children, item => {
+          const {
+            name,
+            width,
+            debounceDelay = FAST_DEBOUNCE,
+            onChange: onItemValueChange,
+          } = item.props;
+          return React.cloneElement(item, {
+            width: width || defaultWidth,
+            value: value?.[name],
+            // remove `debounceDelay` from rendered control item props
+            // so React DevTools don't throw a `invalid prop` warning.
+            debounceDelay: undefined,
+            onChange(fieldValue: JsonValue) {
+              // call `onChange` on each FormItem
+              if (onItemValueChange) {
+                onItemValueChange(fieldValue);
+              }
+              // propagate to the form
+              if (!(debounceDelay in debouncedOnChange)) {
+                debouncedOnChange[debounceDelay] = debounce(
+                  onChange,
+                  debounceDelay,
+                );
+              }
+              debouncedOnChange[debounceDelay]({
+                ...value,
+                [name]: fieldValue,
+              });
+            },
+          });
+        }),
+      });
+    }
+    return row;
   });
-
-  it('renders and handles change for a Checkbox control', () => {
-    const handleChange = jest.fn();
-    render(
-      <ControlFormItem
-        name="test-checkbox"
-        label="Test Checkbox"
-        description="This is a test checkbox"
-        width={200}
-        controlType="Checkbox"
-        initialValue={false}
-        onChange={handleChange}
-      />
-    );
-
-    // Check if ControlHeader and Checkbox are rendered
-    expect(screen.getByText('ControlHeader')).toBeInTheDocument();
-    expect(screen.getByText('Test Checkbox')).toBeInTheDocument();
-
-    const checkbox = screen.getByRole('checkbox');
-    fireEvent.click(checkbox);
-
-    expect(handleChange).toHaveBeenCalledWith(true);
-  });
-
-  it('renders and handles change for a Select control', () => {
-    const handleChange = jest.fn();
-    render(
-      <ControlFormItem
-        name="test-select"
-        label="Test Select"
-        description="This is a test select"
-        width={200}
-        controlType="Select"
-        initialValue="initial"
-        onChange={handleChange}
-        cols={['Option 1', 'Option 2']}
-      />
-    );
-
-    // Check if ControlHeader and Select are rendered
-    expect(screen.getByText('ControlHeader')).toBeInTheDocument();
-    const input = screen.getByTestId('mock-control');
-    fireEvent.change(input, { target: { value: 'changed' } });
-
-    expect(handleChange).toHaveBeenCalledWith('changed');
-  });
-
-  it('renders InfoTooltipWithTrigger on hover', () => {
-    render(
-      <ControlFormItem
-        name="test-tooltip"
-        label="Test Tooltip"
-        description="This is a tooltip"
-        width={200}
-        controlType="Checkbox"
-        initialValue={false}
-      />
-    );
-
-    const checkboxLabel = screen.getByText('Test Checkbox');
-    fireEvent.mouseEnter(checkboxLabel);
-
-    expect(screen.getByText('Tooltip')).toBeInTheDocument();
-  });
-});
+  return (
+    <div
+      css={{
+        label: {
+          textTransform: 'uppercase',
+          color: theme.colors.text.label,
+          fontSize: theme.typography.sizes.s,
+        },
+      }}
+    >
+      {updatedChildren}
+    </div>
+  );
+}
