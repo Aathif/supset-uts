08c2cded5a9ef0b444c4c3f9f3829546cce70701
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-29 13:37:51 +0530

    Update ann.js

--- ann.js
+++ ann.js
@@ -3,18 +3,11 @@ import transformProps from './transformProps';
 describe('transformProps', () => {
   it('should transform chartProps correctly', () => {
     const chartProps = {
+      height: 400,
       width: 800,
-      height: 600,
       formData: {
-        colorScheme: 'schemeBlues',
-        linkLength: '5',
-        normalized: false,
-        cumulative: true,
-        globalOpacity: 0.8,
-        xAxisLabel: 'X Axis',
-        yAxisLabel: 'Y Axis',
-        showLegend: true,
-        sliceId: 42,
+        horizonColorScale: 'schemeBlues',
+        seriesHeight: '30',
       },
       queriesData: [
         {
@@ -27,21 +20,14 @@ describe('transformProps', () => {
     };
 
     const expectedTransformedProps = {
-      width: 800,
-      height: 600,
+      colorScale: 'schemeBlues',
       data: [
         { x: 1, y: 2 },
         { x: 2, y: 3 },
       ],
-      binCount: 5,
-      colorScheme: 'schemeBlues',
-      normalized: false,
-      cumulative: true,
-      opacity: 0.8,
-      xAxisLabel: 'X Axis',
-      yAxisLabel: 'Y Axis',
-      showLegend: true,
-      sliceId: 42,
+      height: 400,
+      seriesHeight: 30,
+      width: 800,
     };
 
     const result = transformProps(chartProps);
