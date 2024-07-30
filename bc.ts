85a39cbc5b1d023a4f3b9213f74e0b1404ad6324
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-30 18:42:57 +0530

    Update bc.ts

--- bc.ts
+++ bc.ts
@@ -1,47 +1,14 @@
-// isEqualArray.test.js
-import isEqualArray from './isEqualArray'; // Adjust the import path
+import { rgb } from 'd3-color';
 
-describe('isEqualArray', () => {
-  test('should return true for arrays with the same reference', () => {
-    const arr = [1, 2, 3];
-    expect(isEqualArray(arr, arr)).toBe(true);
-  });
+// eslint-disable-next-line import/prefer-default-export
+export function hexToRGB(
+  hex: string | undefined,
+  alpha = 255,
+): [number, number, number, number] {
+  if (!hex) {
+    return [0, 0, 0, alpha];
+  }
+  const { r, g, b } = rgb(hex);
 
-  test('should return true for both null arrays', () => {
-    expect(isEqualArray(null, null)).toBe(true);
-  });
-
-  test('should return true for both undefined arrays', () => {
-    expect(isEqualArray(undefined, undefined)).toBe(true);
-  });
-
-  test('should return true for equal arrays', () => {
-    const arrA = [1, 2, 3];
-    const arrB = [1, 2, 3];
-    expect(isEqualArray(arrA, arrB)).toBe(true);
-  });
-
-  test('should return false for arrays with different lengths', () => {
-    const arrA = [1, 2, 3];
-    const arrB = [1, 2];
-    expect(isEqualArray(arrA, arrB)).toBe(false);
-  });
-
-  test('should return false for arrays with different elements', () => {
-    const arrA = [1, 2, 3];
-    const arrB = [1, 2, 4];
-    expect(isEqualArray(arrA, arrB)).toBe(false);
-  });
-
-  test('should return false for one null and one array', () => {
-    const arr = [1, 2, 3];
-    expect(isEqualArray(arr, null)).toBe(false);
-    expect(isEqualArray(null, arr)).toBe(false);
-  });
-
-  test('should return false for one undefined and one array', () => {
-    const arr = [1, 2, 3];
-    expect(isEqualArray(arr, undefined)).toBe(false);
-    expect(isEqualArray(undefined, arr)).toBe(false);
-  });
-});
+  return [r, g, b, alpha];
+}
