db0025256b5a0325f834fc1fe73365280635c88b
Sheikh Athif <mohd.athif24@gmail.com>
2024-07-30 13:35:54 +0530

    Update ann.js

--- ann.js
+++ ann.js
@@ -1,159 +1,420 @@
-import {
-  getMetricLabel,
-  getNumberFormatter,
-  getTimeFormatter,
-  getTimeFormatterForGranularity,
-  NumberFormats,
-  QueryMode,
-  smartDateFormatter,
-  TimeFormats,
-} from '@superset-ui/core';
-import transformProps from './transformProps';
-import { TableChartProps } from './types';
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-jest.mock('@superset-ui/core', () => ({
-  ...jest.requireActual('@superset-ui/core'),
-  getMetricLabel: jest.fn(x => x),
-  getNumberFormatter: jest.fn(() => jest.fn()),
-  getTimeFormatter: jest.fn(() => jest.fn()),
-  getTimeFormatterForGranularity: jest.fn(() => jest.fn()),
-}));
+import React, { useCallback, useMemo, useState } from 'react';
+import { extent as d3Extent, max as d3Max } from 'd3-array';
+import { FaSort } from '@react-icons/all-files/fa/FaSort';
+import { FaSortDown as FaSortDesc } from '@react-icons/all-files/fa/FaSortDown';
+import { FaSortUp as FaSortAsc } from '@react-icons/all-files/fa/FaSortUp';
 
-describe('transformProps', () => {
-  const mockChartProps = (): TableChartProps => ({
-    height: 400,
-    width: 400,
-    datasource: {
-      columnFormats: {},
-      verboseMap: {},
-    },
-    rawFormData: {
-      table_timestamp_format: TimeFormats.DATABASE_DATETIME,
-      time_grain_sqla: 'P1D',
-      metrics: [],
-      percent_metrics: [],
-      column_config: {},
-      align_pn: true,
-      color_pn: true,
-      show_cell_bars: true,
-      include_search: false,
-      page_length: null,
-      table_filter: false,
-      server_pagination: false,
-      export_all_data: false,
-      server_page_length: 10,
-      order_desc: false,
-      query_mode: QueryMode.Aggregate,
-      show_totals: false,
-      cellBgColor: {},
-      columnNameAliasing: {},
-      slice_id: '123',
-    },
-    queriesData: [
-      {
-        data: [],
-        colnames: [],
-        coltypes: [],
+import { GenericDataType, t, tn } from '@superset-ui/core';
+import DataTable from './DataTable';
+import Styles from './Styles';
+import { formatColumnValue } from './utils/formatValue';
+import { PAGE_SIZE_OPTIONS } from './consts';
+import { updateExternalFormData } from './DataTable/utils/externalAPIs';
+import { jsx as ___EmotionJSX } from "@emotion/react";
+import { Tooltip } from 'antd';
+import CustomToolTip from '../../../../CustomTooltip'
+
+/**
+ * Return sortType based on data type
+ */
+function getSortTypeByDataType(dataType) {
+  if (dataType === GenericDataType.Temporal) {
+    return 'datetime';
+  }
+
+  if (dataType === GenericDataType.String) {
+    return 'alphanumeric';
+  }
+
+  return 'basic';
+}
+/**
+ * Cell background to render columns as horizontal bar chart
+ */
+
+
+function cellBar({
+  value,
+  bgColor,
+  valueRange,
+  colorPositiveNegative = false,
+  alignPositiveNegative
+}) {
+  const [minValue, maxValue] = valueRange;
+  const r = colorPositiveNegative && value < 0 ? 150 : 0;
+
+  if (alignPositiveNegative) {
+    const perc = Math.abs(Math.round(value / maxValue * 100)); // The 0.01 to 0.001 is a workaround for what appears to be a
+    // CSS rendering bug on flat, transparent colors
+
+    if(bgColor) {
+      return `linear-gradient(to left, ${bgColor}, ${bgColor} ${perc}%, ` + `rgba(0,0,0,0.01) ${perc}%, rgba(0,0,0,0.001) 100%)`;
+    }
+    return `linear-gradient(to left, rgba(${r},0,0,0.2), rgba(${r},0,0,0.2) ${perc}%, ` + `rgba(0,0,0,0.01) ${perc}%, rgba(0,0,0,0.001) 100%)`;
+  }
+
+  const posExtent = Math.abs(Math.max(maxValue, 0));
+  const negExtent = Math.abs(Math.min(minValue, 0));
+  const tot = posExtent + negExtent;
+  const perc1 = Math.round(Math.min(negExtent + value, negExtent) / tot * 100);
+  const perc2 = Math.round(Math.abs(value) / tot * 100); // The 0.01 to 0.001 is a workaround for what appears to be a
+  // CSS rendering bug on flat, transparent colors
+
+  // 
+  if(bgColor) {
+  return `linear-gradient(to left, ${bgColor}, ${bgColor} ${perc1}%, ` + `rgba(${r},0,0,0.2) ${perc1}%, rgba(${r},0,0,0.2) ${perc1 + perc2}%, ` + `rgba(0,0,0,0.01) ${perc1 + perc2}%, rgba(0,0,0,0.001) 100%)`;
+  }
+  return `linear-gradient(to left, rgba(0,0,0,0.01), rgba(0,0,0,0.001) ${perc1}%, ` + `rgba(${r},0,0,0.2) ${perc1}%, rgba(${r},0,0,0.2) ${perc1 + perc2}%, ` + `rgba(0,0,0,0.01) ${perc1 + perc2}%, rgba(0,0,0,0.001) 100%)`;
+}
+
+function SortIcon({
+  column
+}) {
+  const {
+    isSorted,
+    isSortedDesc
+  } = column;
+
+  let sortIcon = ___EmotionJSX(FaSort, null);
+
+  if (isSorted) {
+    sortIcon = isSortedDesc ? ___EmotionJSX(FaSortDesc, null) : ___EmotionJSX(FaSortAsc, null);
+  }
+
+  return sortIcon;
+}
+
+function SearchInput({
+  count,
+  value,
+  onChange
+}) {
+  return ___EmotionJSX("span", {
+    className: "dt-global-filter"
+  }, ' ', ___EmotionJSX("input", {
+    className: "form-control input-sm",
+    placeholder: 'Search',
+    value: value,
+    onChange: onChange
+  }));
+}
+
+function SelectPageSize({
+  options,
+  current,
+  onChange
+}) {
+  return ___EmotionJSX("span", {
+    className: "dt-select-page-size form-inline"
+  }, t('page_size.show'), ' ', ___EmotionJSX("select", {
+    className: "form-control input-sm",
+    value: current,
+    onBlur: () => {},
+    onChange: e => {
+      onChange(Number(e.target.value));
+    }
+  }, options.map(option => {
+    const [size, text] = Array.isArray(option) ? option : [option, option];
+    return ___EmotionJSX("option", {
+      key: size,
+      value: size
+    }, text);
+  })), ' ', t('page_size.entries'));
+}
+
+export default function TableChart(props) {
+  const {
+    height,
+    width,
+    data,
+    totals,
+    isRawRecords,
+    rowCount = 0,
+    columns: columnsMeta,
+    alignPositiveNegative: defaultAlignPN = false,
+    colorPositiveNegative: defaultColorPN = false,
+    includeSearch = false,
+    pageSize = 0,
+    serverPagination = false,
+    exportAllData = false,
+    serverPaginationData,
+    setDataMask,
+    showCellBars = true,
+    emitFilter = false,
+    sortDesc = false,
+    onChangeFilter,
+    metrics,
+    filters: initialFilters = {},
+    sticky = true, // whether to use sticky header
+    cellBgColor = '',
+    columnNameAliasing = '',
+    sliceId
+  } = props;
+  const [filters, setFilters] = useState(initialFilters);
+  const handleChange = useCallback(filters => {
+    if (!emitFilter) {
+      return;
+    }
+  
+    const groupBy = Object.keys(filters);
+    const groupByValues = Object.values(filters);
+    setDataMask({
+      extraFormData: {
+        filters: groupBy.length === 0 ? [] : groupBy.map(col => {
+          const val = filters == null ? void 0 : filters[col];
+          if (val === null || val === undefined) return {
+            col,
+            op: 'IS NULL'
+          };
+          return {
+            col,
+            op: 'IN',
+            val: val
+          };
+        })
       },
-      {
-        data: [],
+      filterState: {
+        value: groupByValues.length ? groupByValues : null
+      }
+    });
+  }, [emitFilter, setDataMask]); // only take relevant page size options
+
+  const pageSizeOptions = useMemo(() => {
+    const getServerPagination = n => n <= rowCount;
+
+    return PAGE_SIZE_OPTIONS.filter(([n]) => serverPagination ? getServerPagination(n) : n <= 2 * data.length);
+  }, [data.length, rowCount, serverPagination]);
+  const getValueRange = useCallback(function getValueRange(key, alignPositiveNegative) {
+    var _data$;
+
+    if (typeof (data == null ? void 0 : (_data$ = data[0]) == null ? void 0 : _data$[key]) === 'number') {
+      const nums = data.map(row => row[key]);
+      return alignPositiveNegative ? [0, d3Max(nums.map(Math.abs))] : d3Extent(nums);
+    }
+
+    return null;
+  }, [data]);
+  const isActiveFilterValue = useCallback(function isActiveFilterValue(key, val) {
+    var _filters$key;
+
+    return !!filters && ((_filters$key = filters[key]) == null ? void 0 : _filters$key.includes(val));
+  }, [filters]);
+  const toggleFilter = useCallback(function toggleFilter(key, val) {
+    const updatedFilters = { ...(filters || {})
+    };
+
+    if (filters && isActiveFilterValue(key, val)) {
+      updatedFilters[key] = filters[key].filter(x => x !== val);
+    } else {
+      updatedFilters[key] = [...((filters == null ? void 0 : filters[key]) || []), val];
+    }
+
+    if (Array.isArray(updatedFilters[key]) && updatedFilters[key].length === 0) {
+      delete updatedFilters[key];
+    }
+
+    setFilters(updatedFilters);
+    handleChange(updatedFilters);
+  }, [filters, handleChange, isActiveFilterValue]);
+
+  const getSharedStyle = column => {
+    const {
+      isNumeric,
+      config = {}
+    } = column;
+    const textAlign = config.horizontalAlign ? config.horizontalAlign : isNumeric ? 'right' : 'left';
+    return {
+      textAlign
+    };
+  };
+
+  const hiddenColumns = [];
+
+  const getColumnConfigs = useCallback((column, i) => {
+    const {
+      key,
+      label,
+      isNumeric,
+      dataType,
+      isMetric,
+      config = {},
+      bgColor,
+      aliasTitle
+    } = column;
+    const isFilter = !isNumeric && emitFilter;
+    const columnWidth = Number.isNaN(Number(config.columnWidth)) ? config.columnWidth : Number(config.columnWidth); // inline style for both th and td cell
+
+    const sharedStyle = getSharedStyle(column);
+    const alignPositiveNegative = config.alignPositiveNegative === undefined ? defaultAlignPN : config.alignPositiveNegative;
+    const colorPositiveNegative = config.colorPositiveNegative === undefined ? defaultColorPN : config.colorPositiveNegative;
+    const valueRange = (config.showCellBars === undefined ? showCellBars : config.showCellBars) && (isMetric || isRawRecords) && getValueRange(key, alignPositiveNegative);
+    const tooltipColumns = config && config?.tooltipColumns || [];
+    let className = '';
+    if (config?.hideColumn) {
+      hiddenColumns.push(key);
+    }
+
+    if (isFilter) {
+      className += ' dt-is-filter';
+    }
+    return {
+      id: String(key),
+      // to allow duplicate column keys
+      // must use custom accessor to allow `.` in column names
+      // typing is incorrect in current version of `@types/react-table`
+      // so we ask TS not to check.
+      accessor: datum => datum[key],
+      Cell: ({
+        value, row
+      }) => {
+        const [isHtml, text] = formatColumnValue(column, value);
+        const html = isHtml ? {
+          __html: text
+        } : undefined;
+        const cellProps = {
+          // show raw number in title in case of numeric values
+          title: typeof value === 'number' ? String(value) : undefined,
+          onClick: emitFilter && !valueRange ? () => toggleFilter(key, value) : undefined,
+          className: [className, value == null ? 'dt-is-null' : '', isActiveFilterValue(key, value) ? ' dt-is-active-filter' : ''].join(' '),
+          style: { ...sharedStyle,
+            float: (typeof value === 'number' || valueRange) ? 'right' : 'left',
+            textAlign: (typeof value === 'number' || valueRange) ? 'right' : 'left',
+            background: valueRange ? cellBar({
+              value: value,
+              bgColor:  bgColor && valueRange ? bgColor : undefined,
+              valueRange,
+              alignPositiveNegative,
+              colorPositiveNegative
+            }) : undefined
+          }
+        };
+
+        if (html) {
+          // eslint-disable-next-line react/no-danger
+          return ___EmotionJSX("td", _extends({}, cellProps, {
+            dangerouslySetInnerHTML: html
+          }));
+        } // If cellProps renderes textContent already, then we don't have to
+        // render `Cell`. This saves some time for large tables.
+        
+        let tooltipText = tooltipColumns.map((columns, index) => ___EmotionJSX(tooltipColumns.length === (index + 1) ? "span" : "p", {}, `${row?.original?.[columns]}`))
+        if (tooltipText && tooltipText.length > 0) {
+          return ___EmotionJSX("td", cellProps, ___EmotionJSX(Tooltip, {placement: 'right', title:tooltipText}, text));
+        }
+        return ___EmotionJSX("td", cellProps, text);
       },
-    ],
-    initialValues: {},
-    ownState: {},
-    hooks: {
-      onAddFilter: jest.fn(),
-      setDataMask: jest.fn(),
-    },
-  });
-
-  it('should transform props correctly', () => {
-    const chartProps = mockChartProps();
-    const result = transformProps(chartProps);
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        height: chartProps.height,
-        width: chartProps.width,
-        isRawRecords: false,
-        data: [],
-        totals: undefined,
-        columns: [],
-        serverPagination: false,
-        exportAllData: false,
-        metrics: [],
-        percentMetrics: [],
-        serverPaginationData: {},
-        setDataMask: expect.any(Function),
-        alignPositiveNegative: true,
-        colorPositiveNegative: true,
-        showCellBars: true,
-        sortDesc: false,
-        includeSearch: false,
-        rowCount: 0,
-        pageSize: 0,
-        filters: {},
-        emitFilter: false,
-        onChangeFilter: expect.any(Function),
-        cellBgColor: {},
-        columnNameAliasing: {},
-        sliceId: '123',
+      Header: ({
+        column: col,
+        onClick,
+        style
+      }) => ___EmotionJSX("th", {
+        // title: column?.description ? column?.description : '',
+        className: [className, col.isSorted ? 'is-sorted' : ''].join(' '),
+        style: { ...sharedStyle,
+          ...style
+        },
+        onClick: onClick
+      }, config.columnWidth ? // column width hint
+      ___EmotionJSX("div", {
+        style: {
+          width: columnWidth,
+          height: 0.01
+        }
+      }) : null,
+      column?.description ?
+      ___EmotionJSX("span", {
+          
+      }, (aliasTitle || label), 
+      ___EmotionJSX("span", { style: {whiteSpace: 'nowrap'}}, ' '), ___EmotionJSX(CustomToolTip, {
+        className:"m-r-5 text-muted",
+        tooltip:column?.description,
+        label:aliasTitle || label,
+        placement:"right",
+        style: {whiteSpace: 'nowrap'}
       })
-    );
-  });
+     ): 
+      ___EmotionJSX("span", {}, (aliasTitle || label)),
+      ___EmotionJSX(SortIcon, {
+        column: col
+      })),
+      Footer: totals ? i === 0 ? ___EmotionJSX("th", null, t('Totals')) : ___EmotionJSX("td", {
+        style: sharedStyle
+      }, ___EmotionJSX("strong", null, formatColumnValue(column, totals[key])[1])) : undefined,
+      sortDescFirst: sortDesc,
+      sortType: getSortTypeByDataType(dataType)
+    };
+  }, [defaultAlignPN, defaultColorPN, emitFilter, getValueRange, isActiveFilterValue, isRawRecords, showCellBars, sortDesc, toggleFilter, totals, hiddenColumns]);
 
-  it('should handle server pagination correctly', () => {
-    const chartProps = mockChartProps();
-    chartProps.rawFormData.server_pagination = true;
-    chartProps.queriesData = [
-      {
-        data: [],
-      },
-      {
-        data: [{ rowcount: 100 }],
-      },
-    ];
 
-    const result = transformProps(chartProps);
+  // Cell bar color custiomization
+  let columnsMetaNew = columnsMeta;
+  const customColor = {}
+  if(cellBgColor) {
+    const colorArr = cellBgColor.split(',');
+    colorArr && colorArr !== '' && colorArr.forEach((item = '') => {
+      const [colName = '', colColor = ''] = item.split(':')
+      customColor[colName.trim()] = colColor.trim();
+    })
+    columnsMetaNew = columnsMeta.map((item) => {
+      if(customColor[item.label]) {
+        return {...item, bgColor: customColor[item.label]};
+      }
+      return item;
+    })
+  }
 
-    expect(result.rowCount).toBe(100);
-    expect(result.pageSize).toBe(10);
-  });
+  // Aliasing column name custiomization
+  const customAlias = {}
+  if(columnNameAliasing) {
+    const columnAliasArr = columnNameAliasing.split(',');
+    columnAliasArr && columnAliasArr !== '' && columnAliasArr.forEach((item = '') => {
+      const [colName = '', colAlias = ''] = item.split(':')
+      customAlias[colName.trim()] = colAlias.trim();
+    })
+    columnsMetaNew = columnsMetaNew.map((item) => {
+      if(customAlias  [item.label]) {
+        return {...item, aliasTitle: customAlias[item.label]};
+      }
+      return item;
+    })
+  }
 
-  it('should handle totals when show_totals is true and query_mode is Aggregate', () => {
-    const chartProps = mockChartProps();
-    chartProps.rawFormData.show_totals = true;
-    chartProps.rawFormData.query_mode = QueryMode.Aggregate;
-    chartProps.queriesData = [
-      {
-        data: [],
-      },
-      {
-        data: [{ total: 100 }],
-      },
-    ];
+  const columns = useMemo(() => columnsMetaNew.map(getColumnConfigs), [columnsMetaNew, cellBgColor, getColumnConfigs]);
 
-    const result = transformProps(chartProps);
+  const handleServerPaginationChange = (pageNumber, pageSize, filterValue='') => {
+    if (typeof filterValue != "string") {
+      filterValue = '';
+    }
+    updateExternalFormData(setDataMask, pageNumber, pageSize, filterValue);
+  };
 
-    expect(result.totals).toEqual({ total: 100 });
-  });
 
-  it('should handle different page length values correctly', () => {
-    const chartProps = mockChartProps();
-    chartProps.rawFormData.page_length = '20';
-
-    const result = transformProps(chartProps);
-
-    expect(result.pageSize).toBe(20);
-  });
-
-  it('should handle undefined data and columns', () => {
-    const chartProps = mockChartProps();
-    chartProps.queriesData = [];
-
-    const result = transformProps(chartProps);
-
-    expect(result.data).toEqual([]);
-    expect(result.columns).toEqual([]);
-  });
-});
+  return ___EmotionJSX(Styles, null, ___EmotionJSX(DataTable, {
+    columns: columns,
+    data: data,
+    rowCount: rowCount,
+    tableClassName: "table table-striped table-condensed",
+    pageSize: pageSize,
+    serverPaginationData: serverPaginationData,
+    pageSizeOptions: pageSizeOptions,
+    width: width,
+    height: height,
+    onChangeFilter: onChangeFilter,
+    metrics,
+    serverPagination: serverPagination,
+    exportAllData:exportAllData,
+    hiddenColumns: hiddenColumns,
+    onServerPaginationChange: handleServerPaginationChange // 9 page items in > 340px works well even for 100+ pages
+    ,
+    maxPageItemCount: width > 340 ? 9 : 7,
+    noResults: filter => t(filter ? 'No matching records found' : 'No records found'),
+    searchInput: includeSearch && SearchInput,
+    selectPageSize: pageSize !== null && SelectPageSize // not in use in Superset, but needed for unit tests
+    ,
+    sticky: sticky,
+    sliceId: sliceId,
+    setDataMask: setDataMask
+  }));
+}
