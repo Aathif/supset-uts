d2e6c0fea3d81c6e183fd4898febb82c2129466a
Sheikh Athif <mohd.athif24@gmail.com>
2024-08-06 17:18:58 +0530

    Update bc.ts

--- bc.ts
+++ bc.ts
@@ -1,272 +1,287 @@
-import { AnnotationOpacity, getTimeFormatter, isTimeseriesAnnotationResult, smartDateDetailedFormatter, smartDateFormatter } from '@superset-ui/core';
- import { extractForecastSeriesContext } from '../utils/prophet';
- import { ForecastSeriesEnum, LegendOrientation } from '../types';
- import { evalFormula, extractRecordAnnotations, formatAnnotationLabel, parseAnnotationOpacity } from '../utils/annotation';
- import { getChartPadding } from '../utils/series';
- import { TIMESERIES_CONSTANTS } from '../constants';
- export function transformSeries(series, color, opts) {
-   const {
-     name
-   } = series;
-   const {
-     area,
-     forecastEnabled,
-     markerEnabled,
-     markerSize,
-     opacity,
-     seriesType,
-     stack,
-     richTooltip,
-     yAxisIndex = 0
-   } = opts;
-   const forecastSeries = extractForecastSeriesContext(name || '');
-   const isConfidenceBand = forecastSeries.type === ForecastSeriesEnum.ForecastLower || forecastSeries.type === ForecastSeriesEnum.ForecastUpper; // don't create a series if doing a stack or area chart and the result
-   // is a confidence band
- 
-   if ((stack || area) && isConfidenceBand) return undefined;
-   const isObservation = forecastSeries.type === ForecastSeriesEnum.Observation;
-   const isTrend = forecastSeries.type === ForecastSeriesEnum.ForecastTrend;
-   let stackId;
- 
-   if (isConfidenceBand) {
-     stackId = forecastSeries.name;
-   } else if (stack && isObservation) {
-     // the suffix of the observation series is '' (falsy), which disables
-     // stacking. Therefore we need to set something that is truthy.
-     stackId = 'obs' + yAxisIndex;
-   } else if (stack && isTrend) {
-     stackId = forecastSeries.type;
-   }
- 
-   let plotType;
- 
-   if (!isConfidenceBand && (seriesType === 'scatter' || forecastEnabled && isObservation)) {
-     plotType = 'scatter';
-   } else if (isConfidenceBand) {
-     plotType = 'line';
-   } else {
-     plotType = seriesType === 'bar' ? 'bar' : 'line';
-   }
- 
-   const lineStyle = isConfidenceBand ? {
-     opacity: 0
-   } : {};
-   return { ...series,
-     yAxisIndex,
-     name: forecastSeries.name,
-     itemStyle: { color },
-     // @ts-ignore
-     type: plotType,
-     smooth: seriesType === 'smooth',
-     // @ts-ignore
-     step: ['start', 'middle', 'end'].includes(seriesType) ? seriesType : undefined,
-     stack: stackId,
-     lineStyle,
-     areaStyle: {
-       opacity: forecastSeries.type === ForecastSeriesEnum.ForecastUpper || area ? opacity : 0
-     },
-     showSymbol: !isConfidenceBand && (plotType === 'scatter' || forecastEnabled && isObservation || markerEnabled || !richTooltip),
-     // TODO: forcing markers when richTooltip is enabled will be removed once ECharts supports item based tooltips without markers
-     symbolSize: markerSize
-   };
- }
- export function transformFormulaAnnotation(layer, data, supersetColor) {
-   const {
-     name,
-     color,
-     opacity,
-     width,
-     style
-   } = layer;
-   return {
-     name,
-     id: name,
-     itemStyle: { color: supersetColor },
-     lineStyle: {
-       opacity: parseAnnotationOpacity(opacity),
-       type: style,
-       width
-     },
-     type: 'line',
-     smooth: true,
-     data: evalFormula(layer, data),
-     symbolSize: 0,
-     z: 0
-   };
- }
- export function transformIntervalAnnotation(layer, data, annotationData, supersetColor) {
-   const series = [];
-   const annotations = extractRecordAnnotations(layer, annotationData);
-   annotations.forEach(annotation => {
-     const {
-       name,
-       color,
-       opacity
-     } = layer;
-     const {
-       descriptions,
-       intervalEnd,
-       time,
-       title
-     } = annotation;
-     const label = formatAnnotationLabel(name, title, descriptions);
-     const intervalData = [[{
-       name: label,
-       xAxis: time
-     }, {
-       xAxis: intervalEnd
-     }]];
-     series.push({
-       id: `Interval - ${label}`,
-       type: 'line',
-       animation: false,
-       markArea: {
-         silent: false,
-         itemStyle: {
-           color: supersetColor,
-           opacity: parseAnnotationOpacity(opacity || AnnotationOpacity.Medium),
-           emphasis: {
-             opacity: 0.8
-           }
-         },
-         label: {
-           show: false,
-           color: '#000000',
-           // @ts-ignore
-           emphasis: {
-             fontWeight: 'bold',
-             show: true,
-             position: 'insideTop',
-             verticalAlign: 'top',
-             backgroundColor: '#ffffff'
-           }
-         },
-         data: intervalData
-       }
-     });
-   });
-   return series;
- }
- export function transformEventAnnotation(layer, data, annotationData, supersetColor) {
-   const series = [];
-   const annotations = extractRecordAnnotations(layer, annotationData);
-   annotations.forEach(annotation => {
-     const {
-       name,
-       color,
-       opacity,
-       style,
-       width
-     } = layer;
-     const {
-       descriptions,
-       time,
-       title
-     } = annotation;
-     const label = formatAnnotationLabel(name, title, descriptions);
-     const eventData = [{
-       name: label,
-       xAxis: time
-     }];
-     const lineStyle = {
-       width,
-       type: style,
-       color: supersetColor,
-       opacity: parseAnnotationOpacity(opacity),
-       emphasis: {
-         width: width ? width + 1 : width,
-         opacity: 1
-       }
-     };
-     series.push({
-       id: `Event - ${label}`,
-       type: 'line',
-       animation: false,
-       markLine: {
-         silent: false,
-         symbol: 'none',
-         lineStyle,
-         label: {
-           show: false,
-           color: '#000000',
-           position: 'insideEndTop',
-           // @ts-ignore
-           emphasis: {
-             formatter: params => params.name,
-             fontWeight: 'bold',
-             show: true,
-             backgroundColor: '#ffffff'
-           }
-         },
-         data: eventData
-       }
-     });
-   });
-   return series;
- }
- export function transformTimeseriesAnnotation(layer, markerSize, data, annotationData) {
-   const series = [];
-   const {
-     hideLine,
-     name,
-     opacity,
-     showMarkers,
-     style,
-     width
-   } = layer;
-   const result = annotationData[name];
- 
-   if (isTimeseriesAnnotationResult(result)) {
-     result.forEach(annotation => {
-       const {
-         key,
-         values
-       } = annotation;
-       series.push({
-         type: 'line',
-         id: key,
-         name: key,
-         data: values.map(row => [row.x, row.y]),
-         symbolSize: showMarkers ? markerSize : 0,
-         lineStyle: {
-           opacity: parseAnnotationOpacity(opacity),
-           type: style,
-           width: hideLine ? 0 : width
-         }
-       });
-     });
-   }
- 
-   return series;
- }
- export function getPadding(showLegend, legendOrientation, addYAxisLabelOffset, zoomable, margin) {
-   const yAxisOffset = addYAxisLabelOffset ? TIMESERIES_CONSTANTS.yAxisLabelTopOffset : 0;
-   return getChartPadding(showLegend, legendOrientation, margin, {
-     top: TIMESERIES_CONSTANTS.gridOffsetTop + yAxisOffset,
-     bottom: zoomable ? TIMESERIES_CONSTANTS.gridOffsetBottomZoomable : TIMESERIES_CONSTANTS.gridOffsetBottom,
-     left: TIMESERIES_CONSTANTS.gridOffsetLeft,
-     right: showLegend && legendOrientation === LegendOrientation.Right ? 0 : TIMESERIES_CONSTANTS.gridOffsetRight
-   });
- }
- export function getTooltipFormatter(format) {
-   if (format === smartDateFormatter.id) {
-     return smartDateDetailedFormatter;
-   }
- 
-   if (format) {
-     return getTimeFormatter(format);
-   }
- 
-   return String;
- }
- export function getXAxisFormatter(format) {
-   if (format === smartDateFormatter.id || !format) {
-     return undefined;
-   }
- 
-   if (format) {
-     return getTimeFormatter(format);
-   }
- 
-   return String;
- }
+import moment from 'moment';
+import { transformSeries, transformFormulaAnnotation, transformIntervalAnnotation, transformEventAnnotation, transformTimeseriesAnnotation, getPadding, getTooltipFormatter, getXAxisFormatter } from './path/to/your/module'; // Adjust the import path as needed
+import { ForecastSeriesEnum, LegendOrientation } from '../types';
+import { AnnotationOpacity } from '@superset-ui/core';
+
+// Mock dependencies if necessary
+jest.mock('../utils/prophet', () => ({
+  extractForecastSeriesContext: jest.fn(),
+}));
+jest.mock('../utils/annotation', () => ({
+  evalFormula: jest.fn(),
+  extractRecordAnnotations: jest.fn(),
+  formatAnnotationLabel: jest.fn(),
+  parseAnnotationOpacity: jest.fn(),
+}));
+jest.mock('../utils/series', () => ({
+  getChartPadding: jest.fn(),
+}));
+jest.mock('@superset-ui/core', () => ({
+  AnnotationOpacity: {
+    Low: 0.3,
+    Medium: 0.6,
+    High: 0.9,
+  },
+  getTimeFormatter: jest.fn(),
+  isTimeseriesAnnotationResult: jest.fn(),
+  smartDateDetailedFormatter: jest.fn(),
+  smartDateFormatter: {
+    id: 'smart_date',
+  },
+}));
+
+describe('transformSeries', () => {
+  test('transforms series correctly for scatter plot', () => {
+    const series = { name: 'series_name' };
+    const opts = {
+      area: false,
+      forecastEnabled: false,
+      markerEnabled: true,
+      markerSize: 10,
+      opacity: 0.5,
+      seriesType: 'scatter',
+      stack: false,
+      richTooltip: false,
+      yAxisIndex: 0,
+    };
+
+    const result = transformSeries(series, 'red', opts);
+
+    expect(result).toMatchObject({
+      type: 'scatter',
+      smooth: false,
+      symbolSize: 10,
+      showSymbol: true,
+    });
+  });
+
+  test('transforms series correctly for line plot with confidence band', () => {
+    const series = { name: 'series_name' };
+    const opts = {
+      area: false,
+      forecastEnabled: false,
+      markerEnabled: false,
+      markerSize: 0,
+      opacity: 0.5,
+      seriesType: 'line',
+      stack: true,
+      richTooltip: true,
+      yAxisIndex: 0,
+    };
+    extractForecastSeriesContext.mockReturnValue({
+      type: ForecastSeriesEnum.ForecastUpper,
+      name: 'forecast_name',
+    });
+
+    const result = transformSeries(series, 'blue', opts);
+
+    expect(result).toBeUndefined();
+  });
+
+  test('transforms series correctly for stacked bar plot', () => {
+    const series = { name: 'series_name' };
+    const opts = {
+      area: false,
+      forecastEnabled: false,
+      markerEnabled: false,
+      markerSize: 0,
+      opacity: 0.5,
+      seriesType: 'bar',
+      stack: true,
+      richTooltip: true,
+      yAxisIndex: 0,
+    };
+    extractForecastSeriesContext.mockReturnValue({
+      type: ForecastSeriesEnum.Observation,
+      name: 'observation_name',
+    });
+
+    const result = transformSeries(series, 'green', opts);
+
+    expect(result).toMatchObject({
+      type: 'bar',
+      stack: 'obs0',
+    });
+  });
+});
+
+describe('transformFormulaAnnotation', () => {
+  test('transforms formula annotation correctly', () => {
+    const layer = { name: 'formula', color: 'red', opacity: 0.5, width: 2, style: 'solid' };
+    const data = [];
+    const supersetColor = 'blue';
+
+    const result = transformFormulaAnnotation(layer, data, supersetColor);
+
+    expect(result).toMatchObject({
+      name: 'formula',
+      id: 'formula',
+      itemStyle: { color: 'blue' },
+      lineStyle: {
+        opacity: 0.5,
+        type: 'solid',
+        width: 2,
+      },
+      type: 'line',
+      smooth: true,
+    });
+  });
+});
+
+describe('transformIntervalAnnotation', () => {
+  test('transforms interval annotation correctly', () => {
+    const layer = { name: 'interval', color: 'red', opacity: 0.5 };
+    const data = [];
+    const annotationData = [];
+    const supersetColor = 'blue';
+    extractRecordAnnotations.mockReturnValue([{ descriptions: [], intervalEnd: '2023-01-02', time: '2023-01-01', title: 'Title' }]);
+
+    const result = transformIntervalAnnotation(layer, data, annotationData, supersetColor);
+
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      id: 'Interval - interval - Title',
+      type: 'line',
+      markArea: {
+        silent: false,
+        itemStyle: {
+          color: 'blue',
+          opacity: 0.5,
+        },
+        data: [[{ xAxis: '2023-01-01' }, { xAxis: '2023-01-02' }]],
+      },
+    });
+  });
+});
+
+describe('transformEventAnnotation', () => {
+  test('transforms event annotation correctly', () => {
+    const layer = { name: 'event', color: 'red', opacity: 0.5, style: 'solid', width: 2 };
+    const data = [];
+    const annotationData = [];
+    const supersetColor = 'blue';
+    extractRecordAnnotations.mockReturnValue([{ descriptions: [], time: '2023-01-01', title: 'Title' }]);
+
+    const result = transformEventAnnotation(layer, data, annotationData, supersetColor);
+
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      id: 'Event - event - Title',
+      type: 'line',
+      markLine: {
+        silent: false,
+        symbol: 'none',
+        lineStyle: {
+          color: 'blue',
+          opacity: 0.5,
+          type: 'solid',
+          width: 2,
+        },
+        data: [{ xAxis: '2023-01-01' }],
+      },
+    });
+  });
+});
+
+describe('transformTimeseriesAnnotation', () => {
+  test('transforms timeseries annotation correctly', () => {
+    const layer = { name: 'timeseries', hideLine: false, opacity: 0.5, showMarkers: true, style: 'solid', width: 2 };
+    const markerSize = 10;
+    const data = [];
+    const annotationData = { timeseries: [{ key: 'key1', values: [{ x: 1, y: 2 }] }] };
+    isTimeseriesAnnotationResult.mockReturnValue(true);
+
+    const result = transformTimeseriesAnnotation(layer, markerSize, data, annotationData);
+
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      type: 'line',
+      id: 'key1',
+      name: 'key1',
+      data: [[1, 2]],
+      symbolSize: 10,
+      lineStyle: {
+        opacity: 0.5,
+        type: 'solid',
+        width: 2,
+      },
+    });
+  });
+});
+
+describe('getPadding', () => {
+  test('computes padding correctly', () => {
+    const showLegend = true;
+    const legendOrientation = LegendOrientation.Right;
+    const addYAxisLabelOffset = true;
+    const zoomable = true;
+    const margin = { top: 10, bottom: 20, left: 30, right: 40 };
+
+    const result = getPadding(showLegend, legendOrientation, addYAxisLabelOffset, zoomable, margin);
+
+    expect(getChartPadding).toHaveBeenCalledWith(showLegend, legendOrientation, margin, {
+      top: expect.any(Number),
+      bottom: expect.any(Number),
+      left: expect.any(Number),
+      right: expect.any(Number),
+    });
+  });
+});
+
+describe('getTooltipFormatter', () => {
+  test('returns smartDateDetailedFormatter for smart date format', () => {
+    const format = 'smart_date';
+
+    const result = getTooltipFormatter(format);
+
+    expect(result).toBe(smartDateDetailedFormatter);
+  });
+
+  test('returns time formatter for custom format', () => {
+    const format = 'custom_format';
+    const mockFormatter = jest.fn();
+    getTimeFormatter.mockReturnValue(mockFormatter);
+
+    const result = getTooltipFormatter(format);
+
+    expect(result).toBe(mockFormatter);
+    expect(getTimeFormatter).toHaveBeenCalledWith('custom_format');
+  });
+
+  test('returns String formatter for no format', () => {
+    const format = '';
+
+    const result = getTooltipFormatter(format);
+
+    expect(result).toBe(String);
+  });
+});
+
+describe('getXAxisFormatter', () => {
+  test('returns undefined for smart date format', () => {
+    const format = 'smart_date';
+
+    const result = getXAxisFormatter(format);
+
+    expect(result).toBeUndefined();
+  });
+
+  test('returns time formatter for custom format', () => {
+    const format = 'custom_format';
+    const mockFormatter = jest.fn();
+    getTimeFormatter.mockReturnValue(mockFormatter);
+
+    const result = getXAxisFormatter(format);
+
+    expect(result).toBe(mockFormatter);
+    expect(getTimeFormatter).toHaveBeenCalledWith('custom_format');
+  });
+
+  test('returns String formatter for no format', () => {
+    const format = '';
+
+    const result = getXAxisFormatter(format);
+
+    expect(result).toBeUndefined();
+  });
+});
