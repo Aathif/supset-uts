17438d67238685dd46475ad730fb4d3c0761214a
Sheikh Athif <mohd.athif24@gmail.com>
2024-09-19 12:09:42 +0530

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,6 @@
+select d.id, d.dashboard_title, COUNT(logs.dashboard_id) AS "Access Count"
+from logs
+JOIN dashboards d ON logs.dashboard_id = d.id
+JOIN logs l on l.dashboard_id = d.id
+WHERE l.dashboard_id IS NOT NULL and l.action = 'ChartDataRestApi.data'
+GROUP BY d.id, d.dashboard_title;
