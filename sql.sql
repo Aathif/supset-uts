4798fe82e446c89f74006afc4dc13e11938c595a
Sheikh Athif <mohd.athif24@gmail.com>
2024-09-19 12:10:53 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -4,3 +4,6 @@ JOIN dashboards d ON logs.dashboard_id = d.id
 JOIN logs l on l.dashboard_id = d.id
 WHERE l.dashboard_id IS NOT NULL and l.action = 'ChartDataRestApi.data'
 GROUP BY d.id, d.dashboard_title;
+
+
+select id, dashboard_title FROM dashboards;
