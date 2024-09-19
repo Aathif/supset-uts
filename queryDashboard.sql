e9edd624a4d4daee94ac4d98eb65dfa7e6851ab2
Sheikh Athif <mohd.athif24@gmail.com>
2024-09-19 12:32:46 +0530

    Update queryDashboard.sql

--- queryDashboard.sql
+++ queryDashboard.sql
@@ -4,3 +4,16 @@ LEFT JOIN logs l ON d.id = l.dashboard_id
 WHERE l.dashboard_id IS NOT NULL
   AND l.action = 'ChartDataRestApi.data'
 GROUP BY d.id, d.dashboard_title;
+
+
+
+SELECT 
+    d.dashboard_title, 
+    COUNT(DISTINCT l.user_id) AS unique_user_count
+FROM logs l
+JOIN dashboards d ON l.dashboard_id = d.id
+WHERE l.dashboard_id IS NOT NULL 
+  AND l.action = 'ChartDataRestApi.data'
+GROUP BY d.id, d.dashboard_title
+ORDER BY unique_user_count DESC;
+
