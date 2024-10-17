f45c856029d8496f45ab9177ddf67bd09d7d5c73
Sheikh Athif <mohd.athif24@gmail.com>
2024-10-17 12:41:07 +0530

    Update bc.ts

--- bc.ts
+++ bc.ts
@@ -1,58 +1,41 @@
-import replaceUndefinedByNull from './replaceUndefinedByNull';
+import React from 'react';
+import { getNumberFormatter, t, tn } from '@superset-ui/core';
 
-describe('replaceUndefinedByNull', () => {
-  it('should replace undefined with null in a flat object', () => {
-    const input = { key1: undefined, key2: 'value', key3: 123 };
-    const expectedOutput = { key1: null, key2: 'value', key3: 123 };
-    
-    expect(replaceUndefinedByNull(input)).toEqual(expectedOutput);
-  });
+import Label from 'src/components/Label';
+import { Tooltip } from 'src/components/Tooltip';
 
-  it('should replace undefined with null in a nested object', () => {
-    const input = {
-      key1: undefined,
-      key2: { nestedKey1: undefined, nestedKey2: 'value' },
-    };
-    const expectedOutput = {
-      key1: null,
-      key2: { nestedKey1: null, nestedKey2: 'value' },
-    };
-    
-    expect(replaceUndefinedByNull(input)).toEqual(expectedOutput);
-  });
+type RowCountLabelProps = {
+  rowcount?: number;
+  limit?: number;
+  loading?: boolean;
+};
 
-  it('should handle arrays within the object', () => {
-    const input = { key1: [1, undefined, 3], key2: 'value' };
-    const expectedOutput = { key1: [1, null, 3], key2: 'value' };
+const limitReachedMsg = t(
+  'The row limit set for the chart was reached. The chart may show partial data.',
+);
 
-    expect(replaceUndefinedByNull(input)).toEqual(expectedOutput);
-  });
-
-  it('should return a deep clone of the object', () => {
-    const input = { key1: undefined, key2: { nestedKey1: undefined } };
-    const result = replaceUndefinedByNull(input);
-
-    expect(result).toEqual({
-      key1: null,
-      key2: { nestedKey1: null },
-    });
-
-    // Verify that the result is a deep clone
-    result.key2.nestedKey1 = 'new value';
-    expect(input.key2.nestedKey1).toBe(undefined); // original input remains unchanged
-  });
-
-  it('should return an empty object if input is empty', () => {
-    const input = {};
-    const expectedOutput = {};
-    
-    expect(replaceUndefinedByNull(input)).toEqual(expectedOutput);
-  });
-
-  it('should handle no undefined values', () => {
-    const input = { key1: 'value', key2: 123, key3: null };
-    const expectedOutput = { key1: 'value', key2: 123, key3: null };
-    
-    expect(replaceUndefinedByNull(input)).toEqual(expectedOutput);
-  });
-});
+export default function RowCountLabel(props: RowCountLabelProps) {
+  const { rowcount = 0, limit = null, loading } = props;
+  const limitReached = limit && rowcount >= limit;
+  const type =
+    limitReached || (rowcount === 0 && !loading) ? 'danger' : 'default';
+  const formattedRowCount = getNumberFormatter()(rowcount);
+  const label = (
+    <Label type={type}>
+      {loading ? (
+        t('Loading...')
+      ) : (
+        <span data-test="row-count-label">
+          {tn('%s row', '%s rows', rowcount, formattedRowCount)}
+        </span>
+      )}
+    </Label>
+  );
+  return limitReached ? (
+    <Tooltip id="tt-rowcount-tooltip" title={<span>{limitReachedMsg}</span>}>
+      {label}
+    </Tooltip>
+  ) : (
+    label
+  );
+}
