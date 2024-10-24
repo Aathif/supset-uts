eb29253b478bbf8f62deb726945e49039ff059a8
Sheikh Athif <mohd.athif24@gmail.com>
2024-10-24 18:28:38 +0530

    Update bc.ts

--- bc.ts
+++ bc.ts
@@ -1,26 +1,14 @@
-import { setVisibleColumns } from './path_to_setVisibleColumns';
+import { setDatastateFilter } from './path_to_setDatastateFilter';
 
-describe('setVisibleColumns action creator', () => {
-  it('should create an action to set visible columns', () => {
-    const mockPayload = {
-      slice_id: 1,
-      groupBy: ['column1', 'column2'],
-      metrics: ['metric1'],
-      percent_metrics: ['metric2'],
-      allColumns: ['column3', 'column4'],
-    };
+describe('setDatastateFilter action creator', () => {
+  it('should create an action to set the data state filter', () => {
+    const mockDataState = 'ACTIVE';
 
     const expectedAction = {
-      type: 'SET_VISIBLE_COLOUMNS',
-      payload: {
-        slice_id: mockPayload.slice_id,
-        groupBy: mockPayload.groupBy,
-        metrics: mockPayload.metrics,
-        percent_metrics: mockPayload.percent_metrics,
-        allColumns: mockPayload.allColumns,
-      },
+      type: 'SET_DATASTATE',
+      payload: mockDataState,
     };
 
-    expect(setVisibleColumns(mockPayload)).toEqual(expectedAction);
+    expect(setDatastateFilter(mockDataState)).toEqual(expectedAction);
   });
 });
